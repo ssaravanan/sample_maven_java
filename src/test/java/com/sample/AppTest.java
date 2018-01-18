979a10ac7e25f672ded1be33a1668ac2cb05b863
kaushik <kaushik@chef.yourdomain.com>
2018-01-18 13:41:46 +0530

    final

--- src/test/java/com/sample/AppTest.java
+++ src/test/java/com/sample/AppTest.java
@@ -1,8 +1,6 @@
 package com.sample;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,7 +8,6 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
     @Test
 	public void testLengthOfTheUniqueKey() {
